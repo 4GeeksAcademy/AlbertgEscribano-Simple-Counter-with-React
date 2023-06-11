2e0951d0f74af2023bfc004755fa15199318970c
Albert González Escribano <126629570+AlbertgEscribano@users.noreply.github.com>
2023-06-11 00:59:48 +0000

    Final Good One

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -8,53 +8,62 @@ const Counter = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-
-
       if (isCounting) {
-
-      setSeconds((prevSeconds) => prevSeconds + 1);}
-
+        setSeconds((prevSeconds) => prevSeconds + 1);
+        timeOut();
+      }
     }, 1000);
+
+    const timeOut = () => {
+      if (seconds >= 15) {
+        alert("You are out of time. Decrease the counter.");
+      }
+    };
 
     return () => {
       clearInterval(interval);
     };
-  }, [isCounting]);
+  }, [isCounting, seconds]);
+
+  const decrease = () => {
+    if (seconds >= 10 ){
+      setSeconds((prevSeconds) => prevSeconds - 1);
+      timeOut();
+    }
+  };
 
   const handleResume = () => {
-        setIsCounting(true);
-        };
+    setIsCounting(true);
+  };
 
   const handleStop = () => {
-    setIsCounting(false)
+    setIsCounting(false);
   };
 
   const handleStart = () => {
-      setSeconds(0);
-      setIsCounting(true)
+    setSeconds(0);
+    setIsCounting(true);
   };
-
-
 
   return (
     <div>
-        <div className="Counter">
-          <div className="CounterImage">
-            <FontAwesomeIcon icon={faClock} />
-          </div>
-          <div className="five">{Math.floor(seconds / 10000) % 10}</div>
-          <div className="four">{Math.floor(seconds / 1000) % 10}</div>
-          <div className="three">{Math.floor(seconds / 100) % 10}</div>
-          <div className="two">{Math.floor(seconds / 10) % 10}</div>
-          <div className="one">{seconds % 10}</div>
+      <div className="Counter">
+        <div className="CounterImage">
+          <FontAwesomeIcon icon={faClock} />
         </div>
-        <div className='buttonGroup'>
-          <button onClick={handleStop}>Stop</button>
-          <button onClick={handleResume}>Resume</button>
-          <button onClick={handleStart}>Restart</button>
+        <div className="five">{Math.floor(seconds / 10000) % 10}</div>
+        <div className="four">{Math.floor(seconds / 1000) % 10}</div>
+        <div className="three">{Math.floor(seconds / 100) % 10}</div>
+        <div className="two">{Math.floor(seconds / 10) % 10}</div>
+        <div className="one">{seconds % 10}</div>
+      </div>
+      <div className='buttonGroup'>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleResume}>Resume</button>
+        <button onClick={handleStart}>Restart</button>
+        <button onClick={decrease}>Decrease</button>
       </div>
     </div>
-
   );
 };
 
